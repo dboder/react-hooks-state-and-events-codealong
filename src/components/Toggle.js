32ec3f7341d62fc22e9f6f4cc40e8a950709313b
Dorsey B <dorseybec@gmail.com>
2023-04-30 13:20:04 -0700

    complete

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  //useState -  State is data that is dynamic in your component: it changes over time as users interact with your application.
+  const [isOn, setIsOn] = useState(false);
+  const color = isOn ? "red" : "white"; // background color On==Red, Off=White 
+  // ... the rest of Toggle component
+  function handleClick() { // Upon click change state
+    setIsOn((isOn) => !isOn); //  isOn = !isOn; will not work it has to be set using specific set Function
+  }
+  return (<button onClick={handleClick} style={{ background: color }}>{isOn ? "ON" : "OFF"}</button>); // notice the shorthand if statement in that gets printed
 }
 
 export default Toggle;
